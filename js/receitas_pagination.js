f072c71d95fdc03a019cc4d6f51196168a991260
luan-peixoto <66537303+luan-peixoto@users.noreply.github.com>
2021-12-23 18:54:09 -0300

    receitas-responsivo

--- js/receitas_pagination.js
+++ js/receitas_pagination.js
@@ -1,3 +1,45 @@
+
+function show_todos() {
+    $(".todos.fade-in").removeClass("fade-in")
+
+    $(".todos").addClass("fade-out").one('animationend webkitAnimationEnd', function() { 
+        $(".todos").addClass("hide").removeClass("fade-out")
+        $(".todos.hide").removeClass("hide").addClass("fade-in")
+    });
+
+    
+}
+
+function show_familia() {
+    $(".todos.fade-in").removeClass("fade-in")
+
+    $(".todos").addClass("fade-out").one('animationend webkitAnimationEnd', function() { 
+        $(".todos").addClass("hide").removeClass("fade-out")
+        $(".todos.familia.hide").removeClass("hide").addClass("fade-in")
+    });
+}
+
+function show_gourmet() {
+    $(".todos.fade-in").removeClass("fade-in")
+
+    $(".todos").addClass("fade-out").one('animationend webkitAnimationEnd', function() { 
+        $(".todos").addClass("hide").removeClass("fade-out")
+        $(".todos.gourmet.hide").removeClass("hide").addClass("fade-in")
+    });
+}
+
+function show_pratico() {
+    $(".todos.fade-in").removeClass("fade-in")
+
+    $(".todos").addClass("fade-out").one('animationend webkitAnimationEnd', function() { 
+        $(".todos").addClass("hide").removeClass("fade-out")
+        $(".todos.pratico.hide").removeClass("hide").addClass("fade-in")
+    });
+}
+
+
+/*
+
 function show_todos() {
     console.log("todos")
     $(".todos.hide").removeClass("hide").addClass("fade-in").one('animationend webkitAnimationEnd', function() { 
@@ -40,3 +82,5 @@ function show_pratico() {
         $(".todos:not(.pratico)").removeClass("fade-out")
     });
 }
+
+*/
